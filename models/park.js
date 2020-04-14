1343d29efe1b5a0b0b6f06235bee536bdbe03ab4
Ashir <ashirq@outlook.com>
2020-04-14 16:17:32 +0100

    park can remove a dinosaur.

--- models/park.js
+++ models/park.js
@@ -8,4 +8,8 @@ Park.prototype.addDinosaur = function (dinosaur) {
     this.dinosaurs.push(dinosaur)
 }
 
+Park.prototype.removeDinosaur = function(dinosaur){
+  this.dinosaurs.pop();
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -40,7 +40,11 @@ describe('Park', function() {
     assert.deepStrictEqual(actual, [dinosaur1, dinosaur2])
   });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function() {
+    park.removeDinosaur();
+    actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, []);
+  });
 
   it('should be able to find the dinosaur that attracts the most visitors');
 
